bd1d8a8e100462bd3685aa1f39404ea7e1faca0d
Satyakumarmandal <satnarain65@gmail.com>
2025-12-26 00:15:27 +0530

    new js code convertion

---
+++ demo.js
@@ -0,0 +1,38 @@
+//conversion of data types
+let num="";
+let isLogIn=Number(num); 
+// console.log(isLogIn);
+// console.log(typeof (isLogIn));
+// console.log(String(isLogIn));
+// console.log(typeof (String(isLogIn)));
+// console.log(Boolean(num));
+// let age=18;
+// console.log(typeof (String(age)));
+// console.log("satya"+" "+"teja"); //concatination of strings in  case first string id string and second is string with space
+//console.log(12+"22"); //concatination of strings in case both are numbers but in string format
+//console.log(12+ + "22"); //addition of numbers in case both are numbers but in string format
+// exmaple convertion 
+// let priceStr = "₹1999"; 
+// let price = parseInt(priceStr.replace("₹", ""), 10);
+
+// let discountPrice = price * 0.9; // 10% off
+// console.log("Discounted Price:", discountPrice); // 1799.1
+// console.log(typeof discountPrice);
+
+// let dateStr = "2025-12-25";
+// let dateObj = new Date(dateStr);
+
+// console.log(dateObj.toDateString()); // Thu Dec 25 2025
+
+// let isActiveStr = "true";
+// let isActive = (isActiveStr === "true");
+
+// console.log(isActive); // true
+// console.log(typeof isActive); // boolean
+// let jsonData = 
+// '{"name":"Shreyansh","age":22,"isStudent":true}';
+// let userObj = JSON.parse(jsonData);
+
+// console.log(userObj.name); // Shreyansh
+// console.log(userObj.age + 1); // 23
+// console.log(userObj.isStudent); // true 
