4021e6cad217da0ead67a523147f9b84a17c4cae
Satyakumarmandal <satnarain65@gmail.com>
2025-12-28 00:57:04 +0530

    string

---
+++ string.js
@@ -0,0 +1,48 @@
+let firstName="satya";
+let lastName="mandal";
+// string connectated=firstName+" "+lastName;
+// console.log(`my first name ${firstName} and my last name is ${lastName}`);
+// mostly used Backticks for string  template literals  and write variables in ${}
+// and multiple line string also possible with backticks
+// let multiLineString=`this is first line
+// this is second line
+// this is third line`;
+// console.log(multiLineString);
+// kyebord key shift+ ``;
+// let chidAge=`eitheneen`;
+// console.log(`my age is ${chidAge.toUpperCase()} years`);
+// string method used trim() to remove spaces in starting and ending of string
+// const useName="    satnarain  ";
+// console.log(useName);
+// console.log(` ${useName.trim()}`);
+// let text = "satya";
+// console.log(`${text.length}`); //5
+// console.log(`${text.toUpperCase()}`);
+// console.log(`${text.toLowerCase()}`);
+// console.log(`${text.slice(1,4)}`); //aty (1 to 4 index) starting index included and ending index not  add included
+// string methods replace() used to replace part of string with new string
+// const fruitList = "apple, banana, mango, orange, pineapple";
+// console.log(`${fruitList.replace("banana","grapes")}`); //apple, grapes, mango, orange, pineapple
+// toString(); is used to convert other data types to string
+// let accountID=1234567895;
+// let accountIDStr=accountID.toString();
+//  console.log(`Account ID: ${accountIDStr} `); //Account ID: 1234567895
+//  console.log(typeof accountIDStr);
+//  console.log(`Account ID: ${String(accountID)}`); //Account ID: 1234567895  
+// let num = 50;
+// console.log(`${num.toString()}`);        // "50"
+// console.log(typeof num); // string
+let text="satynarain";
+let data="car,bike,bus,train";
+// console.log(`${text.charAt(2)}`);
+// console.log(`${text.indexOf('n')}`); //
+// console.log(`${text.lastIndexOf('i')}`); //4
+// console.log(`${email.includes('65')}`); //true 
+// console.log(`${email.startsWith('satya')}`); //true
+// console.log(text.split(""));
+
+// console.log(`${data.repeat(3)}`);
+
+
+
+
