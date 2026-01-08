9b2faa68b2b9319ac05f94bc074e6c1f933011ec
Satyakumarmandal <satnarain65@gmail.com>
2026-01-09 00:04:43 +0530

    object

--- demo.js
+++ demo.js
@@ -1,142 +1,144 @@
-// //conversion of data types
+// conversion of data types
 // let num="";
 // let isLogIn=Number(num); 
-// // console.log(isLogIn);
-// // console.log(typeof (isLogIn));
-// // console.log(String(isLogIn));
-// // console.log(typeof (String(isLogIn)));
-// // console.log(Boolean(num));
-// // let age=18;
-// // console.log(typeof (String(age)));
-// // console.log("satya"+" "+"teja"); //concatination of strings in  case first string id string and second is string with space
-// //console.log(12+"22"); //concatination of strings in case both are numbers but in string format
-// //console.log(12+ + "22"); //addition of numbers in case both are numbers but in string format
-// // exmaple convertion 
-// // let priceStr = "₹1999"; 
-// // let price = parseInt(priceStr.replace("₹", ""), 10);
+// console.log(isLogIn);
+// console.log(typeof (isLogIn));
+// console.log(String(isLogIn));
+// console.log(typeof (String(isLogIn)));
+// console.log(Boolean(num));
+// let age=18;
+// console.log(typeof (String(age)));
+// console.log("satya"+" "+"teja"); //concatination of strings in  case first string id string and second is string with space
+// console.log(12+"22"); //concatination of strings in case both are numbers but in string format
+// console.log(12+ + "22"); //addition of numbers in case both are numbers but in string format
+// exmaple convertion 
+// let priceStr = "₹1999"; 
+// let price = parseInt(priceStr.replace("₹", ""), 10);
 
-// // let discountPrice = price * 0.9; // 10% off
-// // console.log("Discounted Price:", discountPrice); // 1799.1
-// // console.log(typeof discountPrice);
+// const { use } = require("react");
 
-// // let dateStr = "2025-12-25";
-// // let dateObj = new Date(dateStr);
+// let discountPrice = price * 0.9; // 10% off
+// console.log("Discounted Price:", discountPrice); // 1799.1
+// console.log(typeof discountPrice);
 
-// // console.log(dateObj.toDateString()); // Thu Dec 25 2025
+// let dateStr = "2025-12-25";
+// let dateObj = new Date(dateStr);
 
-// // let isActiveStr = "true";
-// // let isActive = (isActiveStr === "true");
+// console.log(dateObj.toDateString()); // Thu Dec 25 2025
 
-// // console.log(isActive); // true
-// // console.log(typeof isActive); // boolean
-// // let jsonData = 
-// // '{"name":"Shreyansh","age":22,"isStudent":true}';
-// // let userObj = JSON.parse(jsonData);
+// let isActiveStr = "true";
+// let isActive = (isActiveStr === "true");
 
-// // console.log(userObj.name); // Shreyansh
-// // console.log(userObj.age + 1); // 23
-// // console.log(userObj.isStudent); // true 
-// // javaScript data two type permitive and non primitive
-// // primitive types :Class data Number , String , Boolean , Null , Undefined , Symbol , bigInt
-// // this is permitive type  called as value types and cannot changed value( immutable)
-// // non primitive types : object , array , function retunen types = object
-// // function return=functionobject
-// // let age=22; //number
-// // two type of mermory allocation
-// // stack memory - small size data types - permitive types
-// // heap memory - large size data types - non permitive types
-// // stact memory example    
+// console.log(isActive); // true
+// console.log(typeof isActive); // boolean
+// let jsonData = 
+// '{"name":"Shreyansh","age":22,"isStudent":true}';
+// let userObj = JSON.parse(jsonData);
+
+// console.log(userObj.name); // Shreyansh
+// console.log(userObj.age + 1); // 23
+// console.log(userObj.isStudent); // true 
+// javaScript data two type permitive and non primitive
+// primitive types :Class data Number , String , Boolean , Null , Undefined , Symbol , bigInt
+// this is permitive type  called as value types and cannot changed value( immutable)
+// non primitive types : object , array , function retunen types = object
+// function return=functionobject
+// let age=22; //number
+// two type of mermory allocation
+// stack memory - small size data types - permitive types
+// heap memory - large size data types - non permitive types
+// stact memory example    
 // let userName="satya teja"; //string
 // let otherName=userName; //copy by value
 // otherName="shreyansh";
 
-// // console.log(userName); //satya teja
-// // console.log(otherName); //shreyansh
-// // Heap memory example  
-// // heap memory changed ther fore both values are changed  
-// // let user={
-// //     name:"satya teja",
-// //     age:22
-// // };
-// // let otherUser=user; //copy by reference
-// // otherUser.name="shreyansh";
-// // console.log(user.name); //shreyansh
-// // console.log(otherUser.name); //shreyansh    
-// //  const score=100;
-// //  const balance = 11.55
-//     // const score=1000000;
-//     // const name="123.00";
-//     // console.log(parseInt(name));
+// console.log(userName); //satya teja
+// console.log(otherName); //shreyansh
+// Heap memory example  
+// heap memory changed ther fore both values are changed  
+// let user={
+//     name:"satya teja",
+//     age:22
+// };
+// let otherUser=user; //copy by reference
+// otherUser.name="shreyansh";
+// console.log(user.name); //shreyansh
+// console.log(otherUser.name); //shreyansh    
+//  const score=100;
+//  const balance = 11.55
+    // const score=1000000;
+    // const name="123.00";
+    // console.log(parseInt(name));
     
 
 
-// // console.log( balance.toFixed(2).length); // "100.00"  toFixed(2) returns a string with 2 decimal places
+// console.log( balance.toFixed(2).length); // "100.00"  toFixed(2) returns a string with 2 decimal places
 
-// // console.log(balance.toPrecision(3)) ; // "11.6" toPrecision(3) returns a string with 3 significant digits
+// console.log(balance.toPrecision(3)) ; // "11.6" toPrecision(3) returns a string with 3 significant digits
 
-// // console.log(score.toLocaleString('en-IN')); // "1,000" toLocaleString('en-IN') formats the number according to Indian numbering system
+// console.log(score.toLocaleString('en-IN')); // "1,000" toLocaleString('en-IN') formats the number according to Indian numbering system
 
-// // const largeNumber = 123.568
-// // console.log(Math.round(largeNumber)); round to nearest integer
-// // console.log(Math.floor(largeNumber));  flooer below integer
-// // console.log(Math.ceil(largeNumber)); to
-// // const randomNum = Math.random()*10 // random number between 0 .and 1 (exclusive)
-// // console.log(Math.floor(randomNum));
+// const largeNumber = 123.568
+// console.log(Math.round(largeNumber)); round to nearest integer
+// console.log(Math.floor(largeNumber));  flooer below integer
+// console.log(Math.ceil(largeNumber)); to
+// const randomNum = Math.random()*10 // random number between 0 .and 1 (exclusive)
+// console.log(Math.floor(randomNum));
 
-// // console.log(randomNum);
-// // date  Object
-// // const currentDate = new Date();
-// // console.log(currentDate.toDateString()); // e.g., "Mon Jun 1 2026"
+// console.log(randomNum);
+// date  Object
+// const currentDate = new Date();
+// console.log(currentDate.toDateString()); // e.g., "Mon Jun 1 2026"
 
 
-// // currentDate.setFullYear(2026, 2, 0, 14);
-// // console.log(currentDate.toDateString()); // e.g., "Mon Jun 1 2025"
-// // Array is also object type
+// currentDate.setFullYear(2026, 2, 0, 14);
+// console.log(currentDate.toDateString()); // e.g., "Mon Jun 1 2025"
+// Array is also object type
 // let colors=["red","green","blue"];
 // let otherColors=colors;
-// // otherColors.push("yellow"); add is method of array
-// // console.log(otherColors);
-// // otherColors.pop(); last element remove
-// // console.log(otherColors);
-// // otherColors.shift(); first element remove
-// // console.log(otherColors);
-// // otherColors.unshift("pink"); add element at first position
-// // console.log(otherColors);
-// // console.log(colors);
-// // console.log(otherColors);
-// // console.log(Array.isArray(colors)); //true
-// // console.log(colors instanceof Array); //true
-// // console.log(colors.length); //3  
-// // console.log(colors.indexOf("green")); //1
-// // console.log(colors.includes("blue")); //true
-// // console.log(colors.slice(0,2)); //["red","green"] non modified original array  index 0 to index 2-1
-// // console.log(colors.splice(1,1,"yellow")); //["green"] removed element modified original array
-// // console.log(colors); //["red","yellow","blue"]
-// // console.log(colors.join("-")); // "red-green-blue"
+// otherColors.push("yellow"); add is method of array
+// console.log(otherColors);
+// otherColors.pop(); last element remove
+// console.log(otherColors);
+// otherColors.shift(); first element remove
+// console.log(otherColors);
+// otherColors.unshift("pink"); add element at first position
+// console.log(otherColors);
+// console.log(colors);
+// console.log(otherColors);
+// console.log(Array.isArray(colors)); //true
+// console.log(colors instanceof Array); //true
+// console.log(colors.length); //3  
+// console.log(colors.indexOf("green")); //1
+// console.log(colors.includes("blue")); //true
+// console.log(colors.slice(0,2)); //["red","green"] non modified original array  index 0 to index 2-1
+// console.log(colors.splice(1,1,"yellow")); //["green"] removed element modified original array
+// console.log(colors); //["red","yellow","blue"]
+// console.log(colors.join("-")); // "red-green-blue"
 
-// // const student=[
-// //     ["satya",22],
-// //     ["teja",21],
-// //     ["shreyansh",23],
-// //     ["ajay","amit",24,25]
-// // ];
+// const student=[
+//     ["satya",22],
+//     ["teja",21],
+//     ["shreyansh",23],
+//     ["ajay","amit",24,25]
+// ];
 
-// // console.log(student[3][3]); //teja
-// // let accountNuber=[
-// //     [234,567,890],
-// //         [234,567,890],
-// //     [235,567,890],
+// console.log(student[3][3]); //teja
+// let accountNuber=[
+//     [234,567,890],
+//         [234,567,890],
+//     [235,567,890],
 
-// //         [234,567,890],
-// // ];
-// // console.log(accountNuber[3][1]);
+//         [234,567,890],
+// ];
+// console.log(accountNuber[3][1]);
 
-// // let marks = [60, 70, 80];
+// let marks = [60, 70, 80];
 
-// // for (i = 0; i < marks.length; i++) {
+// for (i = 0; i < marks.length; i++) {
 
-// //   console.log(marks[i]);
-// // }
+//   console.log(marks[i]);
+// }
 
 
 // let useName=[22,"satya","teja",true];
@@ -157,3 +159,49 @@
 // console.log(useName);   //["mr.", "satya", "teja", true]
 // console.log(sirName[2]); //n
 // console.log(sirName.charAt(2)); //n
+// signalton Object 
+// let student= new Object() this constracte Object
+// console.log(student);// output empty string{}
+// const student={
+// name: "rohit",
+// address:"chd",
+// mobile:9653464372
+
+// }
+// console.log(Object.keys(student));
+
+// student.name="Shreyansh"; // how to changed key value changed in object 
+// Object.freeze(student) // freeze mean lock your object connont changed key value
+// student.address="mohali";
+// console.log(student);
+// student.myNew = function(){
+//     console.log(`this is my , ${this.name}`);
+    
+// }
+
+// console.log(student.myNew()); call the founction after block
+
+
+
+// let student = {
+//   rollNo: 101,
+//   name: "Amit",
+//   marks: {
+//     math: 80,
+//     science: 75,
+//     english: 85
+//   }
+// };
+
+// console.log(student.marks.math); // 80
+let user = {
+  name: "Satnarain",
+  age: 22,
+  address: {
+    city: "Chandigarh",
+    state: "Punjab",
+    pincode: 160002
+  }
+};
+
+console.log(user); // Chandigarh
